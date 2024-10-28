fdf21278385a15c0aa83e3c5e91b3597e4b3e5ae
caroulinda <145143635+caroulinda@users.noreply.github.com>
2024-10-28 12:02:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    '1.quem sou eu?',
+    '',
+    'sou a ana carolina e gosto de animais'
+)
+
+criaCartao(
+    '2.Minha familia é',
+    '',
+    'minha familia é unida, mas as vezes não'
+)
+
+criaCartao(
+    '3.onde eu moro?',
+    '',
+    'Moro na rua rui barbosa, Bairro lindouro, -25.69222,-51.65689'
+)
+
+criaCartao(
+    '4.Onde estudo?',
+    '',
+    'Colegio Estadual Mario Evaldo Morski. Ele foi criado em 1979 e é a melhor escola do nucleo de guarapuava '
+)
+
+criaCartao(
+    '5.Como é minha turma? ',
+    '',
+    'Minha turma é boa em rendimento e é unida quando tem competições'
+)
+
+criaCartao(
+    '6.Qual faculdade penso em fazer?',
+    '',
+    'Penso em fazer veterinária, por que amo animais'
+)
+
+criaCartao(
+    '7.Como é minha cidade?',
+    '',
+    'Uma cidade meio movimentada com briga, festa, etc. Mas gosto de morar nela'
+)
+
+criaCartao(
+    '8. Quai são os meus sonhos',
+    '',
+    'Construir uma familia, coneguir o trabalho dos sonhos, e dar bem na vida!'
+)
